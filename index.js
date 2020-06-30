7f5346bb4ef874873a1a53e616762cbdee2c3812
lucastobrazil <lucas.arundell@gmail.com>
2020-06-30 10:10:11 +0200

    Add Basic implementation

---
+++ index.js
@@ -0,0 +1,18 @@
+// include node fs module
+var fs = require('fs');
+
+var colors = {
+    primary: '#cc0000',
+    secondary: '#fdfdfd',
+};
+
+var string = '// Design Tokens as SCSS\n';
+
+for (const [key, value] of Object.entries(colors)) {
+    string = `${string}$${key}: ${value};\n`;
+}
+// writeFile function with filename, content and callback function
+fs.writeFile('tokens.scss', string, function (err) {
+    if (err) throw err;
+    console.log('Tokens file created successfully.');
+});
