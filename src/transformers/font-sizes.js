5cf0c23bab190497177b6de2ce986dadb1a50653
lucastobrazil <lucas.arundell@gmail.com>
2020-07-15 17:32:07 +0200

    Add line heights to font sizes export

--- src/transformers/font-sizes.js
+++ src/transformers/font-sizes.js
@@ -1,22 +1,24 @@
 var tokens = require('@adapt-design-system/tokens');
 var makeScssVar = require('../util/scss');
-/*
 
-default = [{
-  name: 'uber',
-  value: 111,
-  unit: 'px',
-  lineHeight: 1.08
-},
-
-*/
+function convertLineHeights() {
+    var fontSizes = tokens.FontSizes;
+    var outputString = '\n// Line Heights\n';
+    fontSizes.map((level) => {
+        outputString += `${makeScssVar(`line-height-${level.name}`, level.lineHeight)}\n`;
+    });
+    return outputString;
+}
 
 function convertFontSizes() {
     var fontSizes = tokens.FontSizes;
     var outputString = '\n// Font Sizes\n';
-    fontSizes.map(level => {
+    fontSizes.map((level) => {
         outputString += `${makeScssVar(`font-size-${level.name}`, `${level.value}${level.unit}`)}\n`;
-    })
+    });
+
+    // Also add line heights
+    outputString += convertLineHeights();
     return outputString;
 }
 
